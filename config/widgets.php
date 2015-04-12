27806bb4038af0c503ad65dc7ddfdbcd352c2bec
David Persson <davidpersson@gmx.de>
2015-04-12 10:02:31 +0200

    Add widgets.

---
+++ config/widgets.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * CMS Event
+ *
+ * Copyright (c) 2013 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+use lithium\g11n\Message;
+use base_core\extensions\cms\Widgets;
+use cms_event\models\Events;
+
+extract(Message::aliases());
+
+Widgets::register('authoring',  function() use ($t) {
+	return [
+		'data' => [
+			$t('Events', ['scope' => 'cms_event']) => Events::find('count')
+		]
+	];
+}, [
+	'type' => Widgets::TYPE_TABLE,
+	'group' => Widgets::GROUP_DASHBOARD,
+	'weight' => Widgets::WEIGHT_HIGH
+]);
+
+?>
