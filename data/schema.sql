f778e737bf176ab647704126d1a1d3b9947a79ba
David Persson <davidpersson@gmx.de>
2015-04-30 17:26:49 +0200

    Update schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `events` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `cover_media_id` int(11) NOT NULL,
+  `cover_media_id` int(11) DEFAULT NULL,
   `title` varchar(250) NOT NULL DEFAULT '',
   `start` date NOT NULL,
   `end` date DEFAULT NULL,
@@ -8,7 +8,12 @@ CREATE TABLE `events` (
   `teaser` text,
   `body` text NOT NULL,
   `tags` varchar(250) DEFAULT NULL,
+  `ticket_url` varchar(250) DEFAULT NULL,
+  `is_sold_out` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `is_published` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `is_published` (`is_published`),
+  KEY `start` (`start`)
+) ENGINE=InnoDB AUTO_INCREMENT=886 DEFAULT CHARSET=utf8;
