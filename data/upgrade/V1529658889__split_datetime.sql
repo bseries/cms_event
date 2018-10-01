4ca5e9e09913c00865f76037feb0dc94fe2952b7
Marius Wilms <mariuswilms@mailbox.org>
2018-10-01 11:26:20 +0200

    Fix migration

--- data/upgrade/V1529658889__split_datetime.sql
+++ data/upgrade/V1529658889__split_datetime.sql
@@ -1,15 +1,15 @@
 ALTER TABLE `events` ADD `start_time` TIME  NULL  AFTER `start`;
 UPDATE events SET start_time = SUBSTRING_INDEX(start, ' ', 2);
+UPDATE events SET start_time = NULL WHERE start = '00:00:00';
 UPDATE events SET start = DATE(start);
 ALTER TABLE events MODIFY start DATE;
-UPDATE events SET start_time = NULL WHERE start = '00:00:00';
 ALTER TABLE `events` CHANGE `start` `start_date` DATE  NOT NULL;
 
 ALTER TABLE `events` ADD `end_time` TIME  NULL  AFTER `end`;
 UPDATE events SET end_time = SUBSTRING_INDEX(end, ' ', 2);
+UPDATE events SET end_time = NULL WHERE end = '00:00:00';
 UPDATE events SET end = DATE(end);
 ALTER TABLE events MODIFY end DATE;
-UPDATE events SET end_time = NULL WHERE start = '00:00:00';
 ALTER TABLE `events` CHANGE `end` `end_date` DATE  NULL  DEFAULT NULL;
 
 ALTER TABLE `events` ADD `timezone` VARCHAR(100)  NULL  DEFAULT 'UTC'  AFTER `end_time`;
